d99c08799969123ffe4399aba8096d42baf186c3
André Junior <andre.souza64@fatec.sp.gov.br>
2021-03-22 13:27:21 -0300

    Desafio terminado

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const isAdmin = this.usersRepository.findById(user_id);
+
+    if (!isAdmin.admin) {
+      throw new Error("Mensagem do erro");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,18 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const findUser = this.usersRepository.findById(user_id);
+
+    if (!findUser) {
+      throw new Error("Uer not found");
+    }
+
+    // findUser.admin = true;
+    // findUser.updated_at = new Date();
+
+    const user = this.usersRepository.turnAdmin(findUser);
+
+    return user;
   }
 }
 
